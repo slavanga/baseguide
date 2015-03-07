d75ff42c57b36fc6524d263eaf5e918565a3d58d
slavanga <lavanga.sergio@gmail.com>
2015-03-08 00:08:34 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# [Baseguide](http://basegui.de)
+Baseguide is a lightweight CSS framework for robust and scalable base styles.
+
+### Features
+* Components scalable via font-size
+* CSS-only custom form controls
+* Vertical rhythm and modular scale built in
+* Bootstrap based grid with extendable breakpoints
+
+### Browser support
+Basic functionality is supported everywhere.
+
+####Custom forms
+The following browsers get the fully enhanced custom form experience:
+* Android 2.3+
+* Chrome
+* Firefox 35+
+* IE 10+
+* Mobile Safari 4+
+* Safari 5.1+
+* Opera 15+
+
+####Autoprefixer Settings
+Baseguide uses [Autoprefixer] (https://github.com/postcss/autoprefixer) with the following settings:
+```
+Android 2.3, Android >= 4, last 4 Chrome versions, Firefox ESR, Explorer >= 8, iOS >= 6, Opera >= 12, Safari >= 6
+```
---
+++ js/helpers.js
@@ -0,0 +1,29 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+	var method;
+	var noop = function () {};
+	var methods = [
+		'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+		'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+		'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+		'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+	];
+	var length = methods.length;
+	var console = (window.console = window.console || {});
+
+	while (length--) {
+		method = methods[length];
+
+		// Only stub undefined methods.
+		if (!console[method]) {
+			console[method] = noop;
+		}
+	}
+}());
+
+
+// empty event handlers for iOS to recognize :active pseudo-classes and label clicks
+if (document.addEventListener) {
+	document.addEventListener('touchstart', function() {}, false);
+	document.addEventListener('click', function() {}, false);
+}
