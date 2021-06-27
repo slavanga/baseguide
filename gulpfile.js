9a4543ff9942c33301a59db0c2796980a4100f3b
slavanga <lavanga.sergio@gmail.com>
2021-06-27 14:20:10 +0200

    Remove scripts and images build steps

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 const gulp = require('gulp');
 const $ = require('gulp-load-plugins')();
-const log = require('fancy-log');
 const fibers = require('fibers');
 const sass = require('gulp-sass');
 const autoprefixer = require('autoprefixer');
@@ -53,44 +52,6 @@ function stylelint() {
     .pipe(gulp.dest(config.src + 'scss'));
 }
 
-// Compile javascript
-function scripts() {
-  return gulp.src(config.src + 'js/*.js')
-    .pipe($.include().on('error', function(error) {
-      log.error(error.message);
-      this.emit('end');
-    }))
-    .pipe($.if(config.sourcemaps, $.sourcemaps.init()))
-    .pipe($.babel())
-    .pipe($.if(config.sourcemaps, $.sourcemaps.write()))
-    .pipe(gulp.dest(config.dest + 'js'))
-    .pipe(browserSync.stream())
-    .pipe($.if(config.minify, $.uglify().on('error', function(error) {
-      log.error(error.message);
-      this.emit('end');
-    })))
-    .pipe($.if(config.sourcemaps, $.sourcemaps.write()))
-    .pipe($.if(config.minify, $.rename({suffix: '.min'})))
-    .pipe($.if(config.minify, gulp.dest(config.dest + 'js')))
-    .pipe(browserSync.stream());
-}
-
-// Lint javascript
-function eslint() {
-  return gulp.src(config.src + 'js/**/*.js')
-    .pipe($.eslint({fix: true}))
-    .pipe($.eslint.format())
-    .pipe(gulp.dest(config.src + 'js'));
-}
-
-// Optimize images
-function images() {
-  return gulp.src(config.src + 'img/**/*.{gif,jpg,png,svg}')
-    .pipe($.cache($.imagemin()))
-    .pipe(gulp.dest(config.dest + 'img'))
-    .pipe(browserSync.stream());
-}
-
 // Serve compiled files
 function serve(done) {
   browserSync.init({
@@ -117,15 +78,13 @@ function serve(done) {
 function watch(done) {
   gulp.watch(config.src + '*.html', html);
   gulp.watch(config.src + 'scss/**/*.scss', styles);
-  gulp.watch(config.src + 'js/**/*.js', scripts);
-  gulp.watch(config.src + 'img/**/*.{gif,jpg,png,svg}', images);
   done();
 }
 
 
-const build = gulp.parallel(html, styles, scripts, images);
+const build = gulp.parallel(html, styles);
 
 exports.build = build;
 exports.watch = watch;
-exports.lint = gulp.parallel(stylelint, eslint);
+exports.lint = stylelint;
 exports.default = gulp.series(build, gulp.parallel(serve, watch));
